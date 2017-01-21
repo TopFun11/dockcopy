d3ae1cb6126ab6f1a58463a3e84f2d8d4c8b6c06
backupa76@gmail.com <backupa76@gmail.com>
2017-01-21 13:43:24 +0000

    wasn't starting the mysql container, suprised nobody noticed

--- start-container.sh
+++ start-container.sh
@@ -1,1 +1,3 @@
+echo "If you get an error about there not being a docker daemon running on your system, look into starting the docker daemon on your system."
+sudo docker start cakephp-mysql
 sudo docker start champions-container
